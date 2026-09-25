3ade7a3315510c186f9f94193b47fbc121c6998d
Adwaith S Raj <rajadwaith4@gmail.com>
2026-09-25 23:39:39 +0530

    feat: rotated half pyramid patterns

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -62,6 +62,23 @@ public class pattern {
         }
     }
 
+    // Rotated Half Pyramid
+static void rotatedHalfPyramid(int n) {
+    for (int i = 1; i <= n; i++) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print("  ");
+        }
+
+        // Print stars
+        for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+    }
+}
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -80,6 +97,9 @@ public class pattern {
 
         System.out.println("\nHollow Rectangle:");
         hollowRectangle(n, n);
+
+        System.out.println("\nRotated Half Pyramid:");
+        rotatedHalfPyramid(n);
 
         sc.close();
     }
