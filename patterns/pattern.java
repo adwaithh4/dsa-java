862b86220837fac113793946851c6fa51742ce20
Adwaith S Raj <rajadwaith4@gmail.com>
2026-09-26 00:08:32 +0530

    feat: 0-1 triangle

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -102,6 +102,19 @@ static void rotatedHalfPyramid(int n) {
         System.out.println();
       }
     }
+    // 8. Zero-One Triangle
+    static void zeroOneTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            if ((i + j) % 2 == 0)
+                System.out.print("1 ");
+            else
+                System.out.print("0 ");
+        }
+        System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -129,6 +142,9 @@ static void rotatedHalfPyramid(int n) {
 
         System.out.println("\nFloyd's Triangle:");
         floydsTriangle(5);
+
+        System.out.println("\nZero-One Triangle:");
+        zeroOneTriangle(5);
         
         sc.close();
     }
