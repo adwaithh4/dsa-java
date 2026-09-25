5d164da2a9ba3f8279f11da1498a3dfa7c5ab442
Adwaith S Raj <rajadwaith4@gmail.com>
2026-09-26 00:39:23 +0530

    feat: butterfly pattern

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -115,6 +115,52 @@ static void rotatedHalfPyramid(int n) {
         }
     }
 
+    //9. Butterfly Pattern
+    static void butterflyPattern(int n) {
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+
+        // Left stars
+        for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+
+        // Spaces
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            System.out.print("  ");
+        }
+
+        // Right stars
+        for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+    }
+
+    // Lower half
+    for (int i = n; i >= 1; i--) {
+
+        // Left stars
+        for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+
+        // Spaces
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            System.out.print("  ");
+        }
+
+        // Right stars
+        for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -145,7 +191,9 @@ static void rotatedHalfPyramid(int n) {
 
         System.out.println("\nZero-One Triangle:");
         zeroOneTriangle(5);
-        
+
+        System.out.println("\nButterfly Pattern:");
+        butterflyPattern(5);
         sc.close();
     }
 }
