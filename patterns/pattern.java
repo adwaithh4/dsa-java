84fd8ab5250579bc6cc5c92584df26833bcddbd4
Adwaith S Raj <rajadwaith4@gmail.com>
2026-09-26 19:48:08 +0530

    feat: diamond pattern

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -200,6 +200,42 @@ static void rotatedHalfPyramid(int n) {
         }
     }
 
+    //12. Diamond Pattern
+    static void diamondPattern(int n) {
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print("  ");
+        }
+
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+    }
+
+    // Lower half
+    for (int i = n - 1; i >= 1; i--) {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print("  ");
+        }
+
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+    }
+}
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -240,7 +276,9 @@ static void rotatedHalfPyramid(int n) {
         System.out.println("\nHollow Rhombus:");
         hollowRhombus(5);
 
-        
+        System.out.println("\nDiamond Pattern:");
+        diamondPattern(5);
+
         sc.close();
 
 
